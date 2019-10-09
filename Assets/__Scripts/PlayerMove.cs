f37cf793c0eaa67eaba5f06bdde9a0cfda32477b
Eoghan O'Connor <oconnorjohn40@gmail.com>
2019-10-09 14:29:24 +0100

    First Commit with scenes Developed and basic Character movement

---
+++ Assets/__Scripts/PlayerMove.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // == Public Properties ==
+    
+    // == Private Variables ==
+    [SerializeField]
+    private float moveSpeed = 10.0f;
+
+    // == Private Methods ==
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        
+    }
+
+    private void Move()
+    {
+    if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0) {
+ 
+    // convert user input into world movement
+    float horizontalMovement = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+    float verticalMovement = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+ 
+     //assign movement to a single vector3
+     Vector3 directionOfMovement = new Vector3(horizontalMovement, verticalMovement, 0);
+ 
+    // apply movement to player's transform
+    gameObject.transform.Translate(directionOfMovement);
+}
+    }
+   
+}
