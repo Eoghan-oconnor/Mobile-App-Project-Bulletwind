130a136fc52ff7da7ed1cff3231fa48e61bdb84f
Eoghan O'Connor <oconnorjohn40@gmail.com>
2019-10-10 09:04:57 +0100

    Adding Working Rotation to Sprite

--- Assets/__Scripts/PlayerMove.cs
+++ Assets/__Scripts/PlayerMove.cs
@@ -13,8 +13,6 @@ public class PlayerMove : MonoBehaviour
     private float moveSpeed = 10.0f;
     private bool direction = true;
     private Rigidbody2D rb;
-    private float _rotation;
-    private float CharacterPosition = 1f;
 
     // == Private Methods ==
     // Update is called once per frame
@@ -40,24 +38,21 @@ public class PlayerMove : MonoBehaviour
             Vector2 movement = new Vector2(horizontalMovement,verticalMovement);
             rb.velocity = movement.normalized * moveSpeed;
 
-            if(Input.GetKeyDown(KeyCode.LeftArrow) && CharacterPosition != 2f)
+            if(Input.GetKeyDown(KeyCode.LeftArrow))
             {
-             transform.Rotate(0,0,90);
-             CharacterPosition = 2f;
+             transform.rotation = Quaternion.Euler(0,0,90);
+            
             }
-            if (Input.GetKeyDown(KeyCode.RightArrow) && CharacterPosition != 3f)
+            if (Input.GetKeyDown(KeyCode.RightArrow))
             {
-                transform.Rotate(0,0, 270);
-                CharacterPosition = 3f;
+                transform.rotation = Quaternion.Euler(0,0, 270);
             }
-            if (Input.GetKeyDown(KeyCode.DownArrow) && CharacterPosition != 4f){
-                transform.Rotate(0,0,180);
-                CharacterPosition = 4f;
+            if (Input.GetKeyDown(KeyCode.DownArrow)){
+                transform.rotation = Quaternion.Euler(0,0,180);
             }
-            if (Input.GetKeyDown(KeyCode.UpArrow) && CharacterPosition != 5f) 
+            if (Input.GetKeyDown(KeyCode.UpArrow)) 
             {
-                transform.Rotate(0,0,360);
-                CharacterPosition = 5f;
+                transform.rotation = Quaternion.Euler(0,0,0);
             }
 
 
