542dabcce71e263e351a7cc6a3a8a32a86b7288e
Eoghan O'Connor <oconnorjohn40@gmail.com>
2019-12-12 13:12:18 +0000

    Doors Destroyed when shot

---
+++ Assets/Door.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D c){
+
+        Destroy(gameObject);
+
+    }
+}
